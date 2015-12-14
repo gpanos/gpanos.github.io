a8a30cd4bbe11d5b32d98d19644997b9fbdee073
Dimitris Karapanos <karapanos.dimtris@gmail.com>
2015-12-14 11:41:42 +0100

    Responsive font sizing

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,7 @@ $( document ).ready( function() {
     workSlide();
     workLoad();
     educationStuff();
+    $("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
 } );
 
 $( 'a[href^="#"]' ).on('click', function(e) {
@@ -85,3 +86,36 @@ $( '#toggle-form').on('click', function(e) {
         
     });
 } );
+
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
